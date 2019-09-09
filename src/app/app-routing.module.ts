eb3541bc7cacba38cf20f91176ab32e6392a1f1c
Revanth <revanthmadasu913@gmail.com>
2019-09-09 12:12:25 +0530

    Working Base Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'signUp',
+    component:SignUpComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
