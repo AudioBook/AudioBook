c0d38b132256bb53d70e9b0d98cf8f5977654c15
Revanth <revanthmadasu913@gmail.com>
2019-09-09 12:14:10 +0530

    Revert "Working Base Created"
    This reverts commit eb3541bc7cacba38cf20f91176ab32e6392a1f1c.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 
 
-const routes: Routes = [
-  {
-    path:'',
-    component:HomeComponent
-  },
-  {
-    path:'signUp',
-    component:SignUpComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
